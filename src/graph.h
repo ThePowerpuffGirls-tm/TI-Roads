1e30b1d1cb4f748095423a0230974d43b50f588a
John Midel Miclat <jmiclat@ufl.edu>
2021-11-15 11:11:14 -0500

    skeleton graph

--- src/graph.h
+++ src/graph.h
@@ -4,10 +4,28 @@
 class Graph
 {
 
+    //Storage for edges and vertices
     std::unordered_map<int, std::vector<int>> g;
+    int numVertices;
 
     public:
+    //ctors
     Graph();
     ~Graph();
 
+    //Other functions
+    void insertEdge(int from, int to);
+    bool isEdge(int from, int to);
+    std::vector<int> getAdjacent(int vertex);
+
 };
+
+Graph::Graph()
+{
+
+}
+
+Graph::~Graph()
+{
+
+}
