0dfbe6a74233a2e62a32deca79fb28cc1928b048
helarious99 <helenahe@ufl.edu>
2021-11-28 13:20:04 -0500

    I accidentally modified main so I changed it back

--- src/graph.h
+++ src/graph.h
@@ -25,10 +25,6 @@ class Graph
     bool isEdge(int from, int to);                                                              //Check?
     std::vector<int> getAdjacent(int vertex);                                                   //Check?
 
-    // Biconnectivity functions
-    bool isBC();
-    bool isConnected();
-    int findAP();
 };
 
 Graph::Graph()
