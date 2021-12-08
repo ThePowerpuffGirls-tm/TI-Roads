fded1b7b83204b8eda7b74817221bfa5ac02d391
Henry Chen <henrychennj@gmail.com>
2021-12-08 13:24:50 -0500

    added weight rng. Commiting to rebase from main

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,25 @@
 #include<iomanip>
 #include<fstream>
 #include<string>
+#include<stdlib.h>
+#include"graph.h"
 using namespace std;
 
-void readFile();
+void readFile(Graph& graph, string& filename);
 
 int main()
 {
-    readFile();
+    Graph graph;
+    string filename = "roadNet-TX.txt";
+    readFile(graph, filename);
+
+
 }
 
-void readFile()
+void readFile(Graph& graph, string& filename)
 {
-    ifstream reader("roadNet-TX.txt");
+    ifstream reader(filename);
 	string newLine = "";
-
 
     //skip the first 4 lines
     for(int i = 0; i < 4; i++)
@@ -24,15 +29,20 @@ void readFile()
     }
 
     int from, to, space;
-    int i = 0;
-    while(reader.is_open())
+    int rngWeight;
+    while(getline(reader, newLine))
     {
-        getline(reader, newLine);
+        rngWeight = rand()%1584+2113;
         space = newLine.find(' ');
         from = stoi(newLine.substr(0, space));
         to = stoi(newLine.substr(space+1));
-
+        if(!graph.isEdge(from, to))
+        {
+            graph.insertEdge(from, to, rngWeight);
+            graph.insertEdge(to, from, rngWeight);
+        }
     }
+
     reader.close();
 
 }
