728852225a2c1e8bd5dadbf511a579ac03e22758
Henry Chen <henrychennj@gmail.com>
2021-12-08 17:20:25 -0500

    removed "using namespace std". Timer code for IO

--- src/main.cpp
+++ src/main.cpp
@@ -3,34 +3,64 @@
 #include<fstream>
 #include<string>
 #include<stdlib.h>
+#include<chrono>
 #include"graph.h"
-using namespace std;
 
-void readFile(Graph& graph, string& filename);
+void readFile(Graph& graph,  std::string& filename);
 
 int main()
 {
     Graph graph;
-    string filename = "roadNet-TX.txt";
+    std::string filename = "roadNet-TX.txt";
     readFile(graph, filename);
 
+    int selection = -1;
+    while(selection != 0)
+    {
 
+        std::cout << "Select algorithm (enter 0 to exit):" << std::endl;
+        std::cout << "1. Dijkstra's Algorithm" << std::endl;
+        std::cout << "2. Bellman-Ford Algorithm" << std::endl;
+        std::cin >> selection;
+
+        if(selection == 1)
+        {
+            //run dijkstras
+        }
+        else if(selection == 2)
+        {
+            //run bellman-ford
+        }
+        else if(selection == 0)
+        {
+            break;
+        }
+        else
+        {
+            std::cout << "Invalid selection" << std::endl;
+        }
+
+        //whitespace/formatting
+        std::cout << selection << std::endl;
+
+    }
 }
 
-void readFile(Graph& graph, string& filename)
+void readFile(Graph& graph, std::string& filename)
 {
-    ifstream reader(filename);
-	string newLine = "";
+    //auto start = std::chrono::high_resolution_clock::now();
+    std::ifstream reader(filename);
+	std::string newLine = "";
 
     //skip the first 4 lines
     for(int i = 0; i < 4; i++)
     {
-        getline(reader, newLine);
+        std::getline(reader, newLine);
     }
 
     int from, to, space;
     int rngWeight;
-    while(getline(reader, newLine))
+    while(std::getline(reader, newLine))
     {
         rngWeight = rand()%1584+2113;
         space = newLine.find(' ');
@@ -42,7 +72,9 @@ void readFile(Graph& graph, string& filename)
             graph.insertEdge(to, from, rngWeight);
         }
     }
-
+    //auto stop = std::chrono::high_resolution_clock::now();
+    //auto duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
+    //std::cout << duration.count() << std::endl;
     reader.close();
 
 }
